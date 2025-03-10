135410b4920bcda9c6cd999439e90d0fba08eac7
Anurudh2030 <anurudhmohan0@gmail.com>
2025-03-10 15:00:12 +0530

    removed all

--- backend/routes/appointmentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-//const appointmentController = require("../controllers/appointmentController");
-const { bookAppointment } = require("./appointmentController");
-router.post("/book", appointmentController.bookAppointment);
-router.get("/", appointmentController.getAppointments);
-
-module.exports = router;
--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Dummy Auth Route
-router.get("/", (req, res) => {
-  res.send("Auth route is working!");
-});
-
-module.exports = router;
